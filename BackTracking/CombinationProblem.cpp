ef06f3ddaca08e8ca75061a0ec0c129f67aed650
ZhihaoWEI <zhihaowei1117@gmail.com>
2023-10-26 22:29:51 +0800

    Update CombinationProblem

--- BackTracking/CombinationProblem.cpp
+++ BackTracking/CombinationProblem.cpp
@@ -35,3 +35,100 @@ private:
 };
 
 // 时间复杂度: O(n * 2^n)  空间复杂度: O(n)
+
+
+// 组合问题2 LeetCode216
+class Solution {
+public:
+
+    void backTracking(int target, int sum, int k, int startIndex){
+        if(k == vec.size()){
+            if(sum == target){
+                ans.emplace_back(vec);
+            }
+            return;
+        }
+
+        // for(int i = startIndex; i <= 9; ++i){
+        //     vec.emplace_back(i);
+        //     sum += i;
+        //     backTracking(target, sum, k, i + 1);
+        //     sum -= i;
+        //     vec.pop_back();
+        // }
+
+        for(int i = startIndex; i <= 9 - k + vec.size() + 1; ++i){
+            vec.emplace_back(i);
+            sum += i;
+
+            if(sum > target){
+                vec.pop_back();
+                sum -= i;
+                return;
+            }
+
+            backTracking(target, sum, k, i + 1);
+            sum -= i;
+            vec.pop_back();
+        }
+
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backTracking(n, 0, k, 1);
+        return ans;
+    }
+
+private:
+    vector<vector<int>> ans;
+    vector<int> vec;
+};
+
+// 时间复杂度: O(n * 2^n)  空间复杂度: O(n)
+
+// 组合问题3 LeetCode 17
+
+class Solution {
+public:
+
+    void backTracking(const string& str, int index){
+        if(str.length() == index){
+            ans.emplace_back(s);
+            return;
+        }
+
+        string sstr = stringMap[str[index] - '0'];
+        for(int i = 0; i < sstr.length(); ++i){
+            s.push_back(sstr[i]);
+            backTracking(str, index + 1);
+            s.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if(digits.length() == 0){
+            return {};
+        }
+        backTracking(digits, 0);
+        return ans;
+    }
+
+private:
+    vector<string> ans;
+    string s;
+    const string stringMap[10] = {
+        "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz"
+    };
+};
+
+// 时间复杂度: O(3^m * 4^n)，其中 m 是对应四个字母的数字个数，n 是对应三个字母的数字个数
+// 空间复杂度: O(3^m * 4^n)
